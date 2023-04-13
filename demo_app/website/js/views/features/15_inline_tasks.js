dbce1cfd73ba5e3ce83601b14fa9dff9a90a7209
Frans King <frans.king@sbbsystems.com>
2023-04-13 20:39:58 +0100

    Added video for inline tasks

--- demo_app/website/js/views/features/15_inline_tasks.js
+++ demo_app/website/js/views/features/15_inline_tasks.js
@@ -6,12 +6,14 @@ export default function ExampleFeature() {
 
   return (
     <div>
-      <Feature title="Inline tasks">
+      <Feature title="Inline tasks" video="/assets/statefun-tasks-inline-tasks-vlc.mp4">
         <Feature.Blurb>
           Task implementations are normally deployed with the workers but in some situations it may be preferred to define a function locally on the client (such as a notebook) and run it as a task.
           The <b>inline_task()</b> decorator in used in place of <b>tasks.bind()</b> for such cases.  
           <br></br><br></br>
           Clearly there are security implications when running code from untrusted sources so this <a href="https://fransking.github.io/flink-statefun-tasks/extensions.html" target="_blank">behaviour</a> is off by default.
+          <br></br><br></br>
+
         </Feature.Blurb>
         <Feature.Code>
 {
@@ -27,7 +29,6 @@ print(result)
 `
 }
         </Feature.Code>
-
         <Feature.Showcase id={uuidv4()} api="/api/inline_tasks/" template={[1]} />
       </Feature>
       <div className="b-example-divider"></div>
