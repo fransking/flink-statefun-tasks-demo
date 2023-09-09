1e97081ad5915cee212a969a66ba1250e4ddc41b
Frans King <frans.king@sbbsystems.com>
2023-09-09 14:33:22 +0100

    Updating Python lib to 1.1.2

--- demo_app/build.sh
+++ demo_app/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-docker build -t fransking/flink-demo-app:1.1.1 .
-docker image inspect fransking/flink-demo-app:1.1.1 --format='{{.Size}}'
+docker build -t fransking/flink-demo-app:1.1.2 .
+docker image inspect fransking/flink-demo-app:1.1.2 --format='{{.Size}}'
