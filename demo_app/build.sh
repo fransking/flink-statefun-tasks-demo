4926e87e6d35ba5e1440e1248a2927355959a106
Frans King <frans.king@sbbsystems.com>
2026-02-14 12:49:21 +0000

    Updating python SDK version

--- demo_app/build.sh
+++ demo_app/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 npm run build
-docker build -t fransking/flink-demo-app:1.2.4 .
-docker image inspect fransking/flink-demo-app:1.2.4 --format='{{.Size}}'
+docker build -t fransking/flink-demo-app:3.4.0 .
+docker image inspect fransking/flink-demo-app:3.4.0 --format='{{.Size}}'
